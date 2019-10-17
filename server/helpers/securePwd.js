d7bbc7215ecb26f32bfbea59ef035726727ea384
Jim Musika Ntare <jimnm2018@gmail.com>
2019-10-17 20:20:30 +0200

    feat(signin-endpoint): create api sign in endpoint (#7)
    create signin method in the userController class
    use bcrypt to decrypt password
    write user sign in tests
    
    [ Finishes #169185988 ]

--- server/helpers/securePwd.js
+++ server/helpers/securePwd.js
@@ -3,3 +3,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const encryptPassword = (password) => bcrypt.hashSync(password, Number(process.env.pwdSalt));
+export const decryptPassword = (password, enteredPwd) => bcrypt.compareSync(password, enteredPwd);
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,5 +5,6 @@ import UserController from "../controllers/userController";
 const authRouter = express.Router();
 
 authRouter.post('/signup', validateUser, UserController.signUp);
+authRouter.post('/signin', UserController.signIn);
 
 export default authRouter;
