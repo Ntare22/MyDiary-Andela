f7211cc8cadded00e69acabdd357efb9ebfe45dd
Ntare22 <jim.musika2016@kepler.org>
2019-10-23 19:51:07 +0200

    feat(modify entry endpoint):
    create modifyEntry method to find an entry's id and modify it
    create patch request in the routes/entryRoutes files and pass in the modifyEntry method
    create tests in the test/b-entrytests.js file to the code
    
    [ Finishes #169204999 ]

--- server/routes/entryRoutes.js
+++ server/routes/entryRoutes.js
@@ -6,5 +6,6 @@ import validateEntry from "../middleware/validateEntry";
 const entryRouter = express.Router();
 
 entryRouter.post('/entries', verifyToken, validateEntry, entryController.createEntry);
+entryRouter.patch('/entries/:entryId', entryController.modifyEntry); 
 
 export default entryRouter;
