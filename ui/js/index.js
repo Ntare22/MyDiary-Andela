ba5427c1ad207d20fab7857629831b93b16ff5c8
Ntare22 <jim.musika2016@kepler.org>
2019-10-25 21:13:33 +0200

    feat(view single entry feature):
    create new page to view single entry
    modify buttons to show only when entry is created
    
    [ Finishes #169353950 ]

--- ui/js/index.js
+++ ui/js/index.js
@@ -24,51 +24,37 @@ window.onclick = (event) => {
     }
 }
 
-/* Redirect to Profile, & entries pages */
-
-document.getElementById("profile-btn").onclick = () => {
-    window.location.href = "./profile.html";
-}
-
+document.getElementById("edit-entry").style.display = "none";
+document.getElementById("delete-entry").style.display = "none";
 
 const saveEntry = () => {
     const entryList = document.getElementById("t-wrap");
     const entryHead = document.getElementById("thought-heading").value;
     let inputElement = document.createElement("input");
     inputElement.type = "checkbox";
-    let entryItem = document.createElement("li");
+    let entryItem = document.createElement("a");
     entryItem.className = "item";
     inputElement.className = "checkbox";
+    entryItem.href = "./single-entry.html"
     // const entryNotes = document.getElementById("thoughts").value;
 
 
-    if (entryHead == "" && entryNotes == "") {
+    if (entryHead == "") {
         console.log("Enter Heading");
     } else {
         entryItem.innerHTML = entryHead;
         entryItem.appendChild(inputElement);
         entryList.appendChild(entryItem);
 
+        document.getElementById("edit-entry").style.display = "block";
+        document.getElementById("delete-entry").style.display = "block";
+
         myPopup.style.display = "none";
     }
 
 }
 
-/***************** Profile *****************/
-
-
-/************* Login ***************/
-
-// const logIn = () => {
-//     console.log("We're logged in")
-// }
+// document.getElementsByClassName("item").onclick = location.href = "./single-entry.html"
 
 
 
-/************* Logging out of myDiary App  */
-
-const logOut = () => {
-    console.log("And we're logged out!!!!")
-}
-
-window.onload = logOut;
