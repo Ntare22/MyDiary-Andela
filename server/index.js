68b683bff3e46a8d8a8adf580f373014637709db
Ntare22 <jim.musika2016@kepler.org>
2019-10-31 12:28:50 +0200

    bg(fix api issues):
    added get request on the index.js file to provide a message.
    
    [ Finishes #169432654 ]

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ import entryRouter from "./routes/entryRoutes";
 const app = express();
 app.use(bodyParser.json());
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1', entryRouter)
+app.use('/api/v1', entryRouter);
 
+
+app.get('/',(req, res) => res.send("Welcome to MyDiary!!"));
 app.use((req, res) => res.status(400).json({
   status: 400,
   error: 'Bad Request'
-}))
+}));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
