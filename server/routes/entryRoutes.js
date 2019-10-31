790850cc93c1c09eb95965baf63e9c515a5f72a2
Ntare22 <jim.musika2016@kepler.org>
2019-10-31 17:11:11 +0200

    fix api issues

--- server/routes/entryRoutes.js
+++ server/routes/entryRoutes.js
@@ -2,14 +2,14 @@ import express from "express";
 import verifyToken from "../middleware/verifyToken";
 import entryController from "../controllers/entryController";
 import validateEntry from "../middleware/validateEntry";
-import verifyParams from "../middleware/verifyParam";
+import validateParams from "../middleware/verifyParam";
 
 const entryRouter = express.Router();
 
 entryRouter.post('/entries', verifyToken, validateEntry, entryController.createEntry);
-entryRouter.patch('/entries/:entryId', verifyParams, verifyToken, entryController.modifyEntry);
-entryRouter.delete('/entries/:entryId', verifyParams, verifyToken, entryController.deleteEntry);
+entryRouter.patch('/entries/:entryId', validateParams, verifyToken, entryController.modifyEntry);
+entryRouter.delete('/entries/:entryId', validateParams, verifyToken, entryController.deleteEntry);
 entryRouter.get('/entries', verifyToken, entryController.viewEntries);
-entryRouter.get('/entries/:entryId', verifyParams, verifyToken, entryController.viewEntry)
+entryRouter.get('/entries/:entryId', validateParams, verifyToken, entryController.viewEntry)
 
 export default entryRouter; 
